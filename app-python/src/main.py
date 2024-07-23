ccd5dda56b017fdb562faa9bb4d1e97e79fb804d
ByWefox <byron.morales@wefox.com>
2024-07-23 20:11:19 +0200

    Merge branch 'feature/APP-CreateFlaskService'

--- app-python/src/main.py
+++ app-python/src/main.py
@@ -1,6 +1,20 @@
+import os
+from flask import Flask, jsonify, url_for, redirect
 
-def main():
-    pass
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    api_methods= {
+        "Get metrics": "GET /metrics"
+    }
+    return jsonify(api_methods)
+
+@app.route('/metrics', methods=['GET'])
+def get_metrics():
+    return jsonify({"data": "hola"})
 
 if __name__ == '__main__':
-    main()
+    port = int(os.environ.get('APP_PORT', 2113))
+    debug= bool(os.environ.get('APP_DEBUG',True))
+    app.run(debug=True, port=port)
